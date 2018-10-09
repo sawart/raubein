8d4fe270214638ba89db15a19f4132f958fc04ca
Rene Giesinger <rene@Renes-iMac.local>
2018-10-09 17:48:25 +0200

    Philosophy / People Finish

--- js/main.js
+++ js/main.js
@@ -5,24 +5,36 @@ $(window).resize(function(){
        if ($(window).width() <= 1024) {  
 
 
-				function siteFooter() {
-					var siteContent = $('.siteContent');
-					var siteContentHeight = siteContent.height();
-					var siteContentWidth = siteContent.width();
+				$(document).ready(function() {
+	
+					// INITIATE THE FOOTER
+				  siteFooter();
+					// COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+				/*	$(window).resize(function() {
+						siteFooter();
+					}); 
+					
+				*/
 
-					var siteFooter = $('.siteFooter');
-					var siteFooterHeight = siteFooter.height();
-					var siteFooterWidth = siteFooter.width();
+					function siteFooter() {
+						var siteContent = $('.siteContent');
+						var siteContentHeight = siteContent.height();
+						var siteContentWidth = siteContent.width();
 
-					console.log('Content Height = ' + siteContentHeight + 'px');
-					console.log('Content Width = ' + siteContentWidth + 'px');
-					console.log('Footer Height = ' + siteFooterHeight + 'px');
-					console.log('Footer Width = ' + siteFooterWidth + 'px');
+						var siteFooter = $('.siteFooter');
+						var siteFooterHeight = siteFooter.height();
+						var siteFooterWidth = siteFooter.width();
 
-					siteContent.css({
-						"margin-bottom" : siteFooterHeight + 50
-					});
-				}
+						console.log('Content Height = ' + siteContentHeight + 'px');
+						console.log('Content Width = ' + siteContentWidth + 'px');
+						console.log('Footer Height = ' + siteFooterHeight + 'px');
+						console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+						siteContent.css({
+							"margin-bottom" : siteFooterHeight + 50
+						});
+					}
+				});
 			});
        }     
 });
