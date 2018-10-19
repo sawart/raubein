e3a280ae486da363683dddf1e9b2aa89a694ae8d
Rene Giesinger <rene@Renes-iMac.local>
2018-10-19 13:01:10 +0200

    Mega Submenu added

---
+++ footer.php
@@ -0,0 +1,62 @@
+	</div>
+</div> <!-- siteContent End -->	
+<div class="siteFooter">
+	<div class="footerItem">
+		<h2>Sitemap</h2>
+		<ul>
+			<li><a href="#">Shop</a>
+				<ul>
+					<li><a href="#">Tische &amp; Stühle</a></li>
+					<li><a href="#">Sofas &amp; Sessel</a></li>
+					<li><a href="#">Schränke &amp; Regale</a></li>
+					<li><a href="#">Lampen &amp; Leuchten</a></li>
+					<li><a href="#">Accessories &amp; Dekoration</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Philosophie</a></li>
+			<li><a href="#">Menschen</a></li>
+			<li><a href="#">Kontakt</a></li>
+		</ul>
+	</div>
+	<div class="footerItem">
+	<h2>Service</h2>
+		<ul>
+			<li><a href="#">Lieferkosten</a></li>
+			<li><a href="#">Mein Konto</a></li>
+			<li><a href="#">Presse</a></li>
+			<li><a href="#">Agb</a></li>
+			<li><a href="#">Impressum</a></li>
+			<li><a href="#">Datenschutz</a></li>
+		</ul>
+	<h2>Zahlungsarten</h2>
+		<div class="payments">
+			<img src="gfx/payment/mastercard.png" alt="Mastercard">
+			<img src="gfx/payment/visa.png" alt="Visa">
+			<img src="gfx/payment/paypal.png" alt="PayPal">
+		</div>
+	</div>
+	<div class="footerItem">
+	<h2>Kontakt</h2>
+		<p>
+		<strong>Raubein - Vintage &amp; Industrie Möbel</strong><br>
+		Mariahilfstrasse 29<br>
+		6900 Bregenz<br>
+		Austria<br>
+		<br>
+		<a href="tel:+436647931641">+43 664 79 31 641</a><br>
+		<br>	
+		<a href="mailto:hello@raubein.at">hello@raubein.at</a><br>
+		</p>
+		<h2>Newsletter</h2>
+		<p>Immer informiert bleiben <br>über neue Produkte, 
+Events &amp; PopUp Stores</p>
+		<input type="text" placeholder="E-Mail Adresse"><input type="submit" value="ok">
+	</div>
+	<div class="copyright">
+	<p>&copy; copyright | Raubein - Vintage &amp; Industrie Möbel - 2018</p>
+	</div>
+</div>	
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="<?php bloginfo('template_url');?>/js/main.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Raubein</title>
+	<link href="<?php bloginfo('template_url');?>/sass/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="siteContent">
+	<div class="parallax">
+		<header>
+			<div style="background: url(<?php bloginfo('template_url');?>/gfx/logo.png) no-repeat; background-size: contain;" class="logo"></div>
+			<nav>
+				<div class="hburger">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				
+				<ul class="navEffect">
+					<li><a href="#">Shop</a></li>
+					<li><a href="#">Philosophie</a></li>
+					<li><a href="#">Menschen</a></li>
+					<li><a href="#">Kontakt</a></li>
+				</ul>
+				
+				<div class="login"><p>Mein Konto</p></div>
+				<div class="search sfill"><span></span></div>
+			</nav>
+			
+			
+
+		</header>	
